8b0155f66106db7f955363ace859495389c6fc1b
k.b.Yudhees Raam Kumar <yudees479@gmail.com>
2024-06-23 20:27:38 +0530

    test commit

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,3 @@ Route::get('/login',[AuthController::class,'login'])->name('login');
 Route::get('/register',[AuthController::class, 'register'])->name('register');
 Route::post('/register-user',[AuthController::class, 'register_user'])->name('register_user');
 Route::post('/login-user',[AuthController::class, 'login_user'])->name('login_user');
-// assertDoesNotMatchRegularExpression
