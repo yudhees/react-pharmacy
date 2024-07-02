9909b9f87f9fc769f6dac5defd667a6b7f8b76fa
k.b.Yudhees Raam Kumar <yudees.raamkumar@bakuun.com>
2024-07-02 11:23:20 +0530

    db setup

--- config/database.php
+++ config/database.php
@@ -41,10 +41,8 @@ return [
         'mongodb' => [
         'driver' => 'mongodb',
         'dsn' => env('DB_URI'),
-        mongodb+srv://Nithish:ntHlYGEFrHN84C6M@cluster0.kspw7gm.mongodb.net/cluster0-db?retryWrites=true&w=majority
-        'database' => 'pharmacy',
-      ],
-
+        'database' =>env('DB_DATABASE'),
+        ],
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
